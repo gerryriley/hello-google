2fb0805e8f6a0d4f4e72cde33718e08857074f7f
gerryriley <gerry@riley1.me.uk>
2017-02-17 18:39:42 +0000

    Initial Setup

---
+++ src/main/java/uk/me/riley1/hellogoogle/HelloAppEngine.java
@@ -0,0 +1,23 @@
+package uk.me.riley1.hellogoogle;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloAppEngine extends HttpServlet {
+
+  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+      
+    response.setContentType("text/plain");
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
---
+++ src/test/java/uk/me/riley1/hellogoogle/HelloAppEngineTest.java
@@ -0,0 +1,17 @@
+package uk.me.riley1.hellogoogle;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloAppEngineTest {
+
+  @Test
+  public void test() throws IOException {
+    MockHttpServletResponse response = new MockHttpServletResponse();
+    new HelloAppEngine().doGet(null, response);
+    Assert.assertEquals("text/plain", response.getContentType());
+    Assert.assertEquals("Hello App Engine!\r\n", response.getWriterContent().toString());
+  }
+}
